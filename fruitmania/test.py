a518d83ee3bf534e9654d1ff696c7e4a9aa84dcd
badnumberr <ymniychel000@gmail.com>
2025-01-19 16:55:51 +0500

    fruitmania

--- fruitmania/test.py
+++ fruitmania/test.py
@@ -12,7 +12,8 @@ player_speed = 12
 player_x = w // 2 - player_size // 2
 player_y = h - player_size - 10
 
-circle_size = 70
+fruits_size = 70
+
 banana_speed = 5
 bomb_speed = 6
 strawberry_speed = 8
@@ -24,8 +25,9 @@ strawberrys = []
 oranges = []
 
 score = 0
-circle_timer = 2500
-last_circle = pygame.time.get_ticks()
+fruits_timer = 2500
+last_fruit = pygame.time.get_ticks()
+contact_distance = 80
 
 banana_image = pygame.image.load("images/banana.png")
 banana_image = pygame.transform.scale(banana_image, (70, 70))
@@ -39,8 +41,6 @@ strawberry_image = pygame.transform.scale(strawberry_image, (70, 70))
 orange_image = pygame.image.load("images/orange.png")
 orange_image = pygame.transform.scale(orange_image, (70, 70))
 
-contact_distance = 80
-
 background_image = pygame.image.load("images/background.png")
 background_image = pygame.transform.scale(background_image, (1200, h))
 
@@ -53,13 +53,13 @@ game_over_image = pygame.transform.scale(game_over_image, (w, h))
 instruction_image = pygame.image.load("images/instruction.png")
 instruction_image = pygame.transform.scale(instruction_image, (600, 800))
 
-settings_image = pygame.image.load("images/settings_button.png")
+settings_image = pygame.image.load("images/instruction_button.png")
 settings_image = pygame.transform.scale(settings_image, (300, 140))
 
 play_image = pygame.image.load("images/play_button.png")
 play_image = pygame.transform.scale(play_image, (300, 140))
 
-instruction_button_image = pygame.image.load("images/instruction_button.png")
+instruction_button_image = pygame.image.load("images/settings_button.png")
 instruction_button_image = pygame.transform.scale(instruction_button_image, (300, 140))
 
 
@@ -78,125 +78,88 @@ def show_instructions():
             a += 5
         else:
             a = 255
-
         instruction_image.set_alpha(a)
-
         screen.blit(instruction_image, (550, 50))
         pygame.display.flip()
-        pygame.time.delay(30)
+        pygame.time.delay(5)
 
-def show_intro():
-    intro_running = True
-    while intro_running:
+
+def start_game():
+    global player_x, player_y, score, bananas, bombs, strawberrys, oranges, last_fruit
+    running = True
+
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            if event.type == pygame.KEYDOWN:
-                intro_running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_x, mouse_y = event.pos
-                if (670 < mouse_x < 870) and (450 < mouse_y < 550):
-                    show_instructions()
+                running = False
 
+        k = pygame.key.get_pressed()
+        if k[pygame.K_LEFT] and player_x > 0:
+            player_x -= player_speed
+        if k[pygame.K_RIGHT] and player_x < 1200 - player_size:
+            player_x += player_speed
 
+        current_time = pygame.time.get_ticks()
+        if current_time - last_fruit >= fruits_timer:
+            last_fruit = current_time
+            x = random.randint(fruits_size, 1150 - fruits_size)
+            y = -fruits_size
 
-        screen.blit(intro_image, (0, 0))
-        screen.blit(settings_image, (670, 450))
-        screen.blit(instruction_button_image, (670, 550))
-        screen.blit(play_image, (670, 350))
+            fruits_type = random.choice(['orange', 'bomb', 'banana', "strawberry"])
+            if fruits_type == 'orange':
+                oranges.append([x, y])
+            elif fruits_type == 'bomb':
+                bombs.append([x, y])
+            elif fruits_type == 'banana':
+                bananas.append([x, y])
+            else:
+                strawberrys.append([x, y])
+
+        handle_fruits(bananas, banana_speed, 10)
+        handle_fruits(bombs, bomb_speed, -15, is_bomb=True)
+        handle_fruits(strawberrys, strawberry_speed, 30)
+        handle_fruits(oranges, oranges_speed, 20)
+
+        draw_game()
         pygame.display.flip()
-        pygame.time.delay(30)
+        pygame.time.delay(5)
+
+    pygame.quit()
 
 
-show_intro()
-
-
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    k = pygame.key.get_pressed()
-    if k[pygame.K_LEFT] and player_x > 0:
-        player_x -= player_speed
-    if k[pygame.K_RIGHT] and player_x < 1200 - player_size:
-        player_x += player_speed
-
-    current_time = pygame.time.get_ticks()
-    if current_time - last_circle >= circle_timer:
-        last_circle = current_time
-        x = random.randint(circle_size, 1150 - circle_size)
-        y = -circle_size
-
-        fruits_type = random.choice(['orange', 'bomb', 'banana', "strawberry"])
-        if fruits_type == 'orange':
-            oranges.append([x, y])
-        elif fruits_type == 'bomb':
-            bombs.append([x, y])
-        elif fruits_type == 'banana':
-            bananas.append([x, y])
-        else:
-            strawberrys.append([x, y])
-
-    for i in range(len(bananas) - 1, -1, -1):
-        bananas[i][1] += banana_speed
-        circle_x = bananas[i][0]
-        circle_y = bananas[i][1]
+def handle_fruits(fruit_list, speed, score_change, is_bomb=False):
+    global score
+    for i in range(len(fruit_list) - 1, -1, -1):
+        fruit_list[i][1] += speed
+        circle_x, circle_y = fruit_list[i]
 
         if (abs(player_x - circle_x) < contact_distance and
                 abs(player_y - circle_y) < contact_distance):
-            score += 10
-            bananas.pop(i)
+            score += score_change
+            fruit_list.pop(i)
+        elif fruit_list[i][1] > h + fruits_size:
+            fruit_list.pop(i)
 
-        elif bananas[i][1] > h + circle_size:
-            bananas.pop(i)
+    for banana in bananas:
+        screen.blit(banana_image, (banana[0], banana[1]))
+    for bomb in bombs:
+        screen.blit(bomb_image, (bomb[0], bomb[1]))
+    for strawberry in strawberrys:
+        screen.blit(strawberry_image, (strawberry[0], strawberry[1]))
+    for orange in oranges:
+        screen.blit(orange_image, (orange[0], orange[1]))
 
-    for i in range(len(bombs) - 1, -1, -1):
-        bombs[i][1] += bomb_speed
-        circle_x = bombs[i][0]
-        circle_y = bombs[i][1]
+    pygame.display.flip()
+    pygame.time.delay(5)
 
-        if (abs(player_x - circle_x) < contact_distance and
-                abs(player_y - circle_y) < contact_distance):
-            score -= 15
-            bombs.pop(i)
 
-        elif bombs[i][1] > h + circle_size:
-            bombs.pop(i)
 
-    for i in range(len(strawberrys) - 1, -1, -1):
-        strawberrys[i][1] += strawberry_speed
-        circle_x = strawberrys[i][0]
-        circle_y = strawberrys[i][1]
-
-        if (abs(player_x - circle_x) < contact_distance and
-                abs(player_y - circle_y) < contact_distance):
-            score += 30
-            strawberrys.pop(i)
-
-        elif strawberrys[i][1] > h + circle_size:
-            strawberrys.pop(i)
-
-    for i in range(len(oranges) - 1, -1, -1):
-        oranges[i][1] += oranges_speed
-        circle_x = oranges[i][0]
-        circle_y = oranges[i][1]
-
-        if (abs(player_x - circle_x) < contact_distance and
-                abs(player_y - circle_y) < contact_distance):
-            score += 20
-            oranges.pop(i)
-
-        elif oranges[i][1] > h + circle_size:
-            oranges.pop(i)
-
+def draw_game():
     screen.fill('black')
     screen.blit(background_image, (0, 0))
     player_image = pygame.image.load("images/player.png")
     player_image = pygame.transform.scale(player_image, (140, 200))
     screen.blit(player_image, (player_x, player_y))
-
     for banana in bananas:
         screen.blit(banana_image, (banana[0], banana[1]))
     for bomb in bombs:
@@ -209,6 +172,36 @@ while running:
     font = pygame.font.Font(None, 55)
     text = font.render(f"Рекорд игрока: {score}", True, 'white')
     screen.blit(text, (1220, 840))
-    pygame.display.flip()
-    pygame.time.delay(20)
+
+
+def show_intro():
+    intro_running = True
+    while intro_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                intro_running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = event.pos
+                if (670 < mouse_x < 970) and (350 < mouse_y < 490):
+                    intro_running = False
+                    start_game()
+                if (670 < mouse_x < 870) and (450 < mouse_y < 550):
+                    font = pygame.font.Font(None, 30)
+                    text = font.render("(Чтобы закрыть инструкцию нажмите любую кнопку на клавиатуре)", True, 'white')
+                    screen.blit(text, (500 , 840))
+                    show_instructions()
+
+
+        screen.blit(intro_image, (0, 0))
+        screen.blit(settings_image, (670, 450))
+        screen.blit(instruction_button_image, (670, 550))
+        screen.blit(play_image, (670, 350))
+
+        pygame.display.flip()
+        pygame.time.delay(5)
+
+show_intro()
 pygame.quit()
